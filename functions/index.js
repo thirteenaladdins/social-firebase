386d31696c4e507694b7c3f0e1c104fd6e082aee
arabaladdin <moamrr@gmail.com>
2020-03-26 13:10:30 +0000

    Modified initial commit

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello, world!");
+  response.send("Hello, world!");
 });
+
+exports.getPosts = functions.https.onRequest((request, response) => {
+  admin.firestore().collection('posts').get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push(doc.data());
+      })
+      return response.json(posts);
+    })
+    .catch(err => console.error(err))
+})
