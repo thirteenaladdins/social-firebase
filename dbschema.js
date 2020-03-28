8af03a5a768c42724befe9771fd5f5978488f36d
arabaladdin <moamrr@gmail.com>
2020-03-28 00:06:53 +0000

    Format for database entries

--- dbschema.js
+++ dbschema.js
@@ -1,9 +1,14 @@
+// properties of a post
+// you are chargd for the amount of reads with firebase
+
 let db = {
   posts: [
     {
       userHandle: 'user',
       body: 'this is the post body',
-      createdAt: 'time post created'
+      createdAt: '2020-03-27T18:44:34.555Z',
+      likeCount: 5,
+      commentCount: 2
     }
   ]
 }
